ce68e5bb7e84393b7ced4143ce566d91b8219d32
nastich01 <cheshirsky_n@mail.ru>
2023-06-09 10:50:47 +0300

    Changes

--- app/src/test/java/com/example/myroute/Tests.java
+++ app/src/test/java/com/example/myroute/Tests.java
@@ -1,4 +1,4 @@
-package com.example.testcovid;
+package com.example.myroute;
 
 import org.junit.Test;
 
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class Tests {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
